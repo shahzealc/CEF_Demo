b5546e246cd8387c30024b56fee9ce356a05ad57
Zeal Shah <zeal532003@gmail.com>
2025-03-21 15:57:21 +0530

    Add all DC urls to test performance while gpu_acceleration is off.

--- Source/CEF_Demo/Private/MainUI.cpp
+++ Source/CEF_Demo/Private/MainUI.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "MainUI.h"
 #include <ChatBrowser.h>
 #include <HomeBrowser.h>
@@ -21,22 +20,48 @@ void UMainUI::NativeOnInitialized()
 		SaveButton->OnClicked.AddUniqueDynamic(this, &UMainUI::handleSaveButtonClick);
 	}
 
-}
-
-void UMainUI::handleChatButtonClick()
-{
-	UE_LOG(LogTemp, Warning, TEXT("Chat"));
-	if (ChatBrowser) {
-		ChatBrowser->LoadURL(TEXT("https://stage-chat.zurutech.online/"));
+	if (StoreButton) {
+		StoreButton->OnClicked.AddUniqueDynamic(this, &UMainUI::handleStoreButtonClick);
 	}
-}
 
-void UMainUI::handleHomeButtonClick()
-{
-	UE_LOG(LogTemp, Warning, TEXT("Home"));
+	if (AccountButton) {
+		AccountButton->OnClicked.AddUniqueDynamic(this, &UMainUI::handleAccountButtonClick);
+	}
 
-	if (HomeBrowser) {
-		HomeBrowser->LoadURL(TEXT("https://stage-home.zurutech.online/?version=504&platform=editor&dcversion=v0.0.1"));
+	if (NotificationButton) {
+		NotificationButton->OnClicked.AddUniqueDynamic(this, &UMainUI::handleNotificationButtonClick);
+	}
+
+	if (PublishButton) {
+		PublishButton->OnClicked.AddUniqueDynamic(this, &UMainUI::handlePublishButtonClick);
+	}
+
+	if (OrderButton) {
+		OrderButton->OnClicked.AddUniqueDynamic(this, &UMainUI::handleOrderButtonClick);
+	}
+
+	if (ProjectHistoryButton) {
+		ProjectHistoryButton->OnClicked.AddUniqueDynamic(this, &UMainUI::handleProjectHistoryButtonClick);
+	}
+
+	if (OrderSummaryButton) {
+		OrderSummaryButton->OnClicked.AddUniqueDynamic(this, &UMainUI::handleOrderSummaryButtonClick);
+	}
+
+	if (ObjectButton) {
+		ObjectButton->OnClicked.AddUniqueDynamic(this, &UMainUI::handleObjectButtonClick);
+	}
+
+	if (MEPButton) {
+		MEPButton->OnClicked.AddUniqueDynamic(this, &UMainUI::handleMEPButtonClick);
+	}
+
+	if (MaterialButton) {
+		MaterialButton->OnClicked.AddUniqueDynamic(this, &UMainUI::handleMaterialButtonClick);
+	}
+
+	if (QeraButton) {
+		QeraButton->OnClicked.AddUniqueDynamic(this, &UMainUI::handleQeraButtonClick);
 	}
 }
 
@@ -44,7 +69,136 @@ void UMainUI::handleSaveButtonClick()
 {
 	UE_LOG(LogTemp, Warning, TEXT("Save %s"), *TokenBox->GetText().ToString());
 	OnAccessToken.ExecuteIfBound(*TokenBox->GetText().ToString());
-	HomeBrowser->HandleOnAccessToken(*TokenBox->GetText().ToString());
-	ChatBrowser->HandleOnAccessToken(*TokenBox->GetText().ToString());
+
+	TArray<UBrowserWidget*> Browsers = { ChatBrowser, HomeBrowser, StoreBrowser, AccountBrowser, NotificationBrowser,
+		PublishBrowser, OrderBrowser, ProjectHistoryBrowser, OrderSummaryBrowser, ObjectBrowser,
+		MEPBrowser, MaterialBrowser, QeraBrowser };
+
+	for (UBrowserWidget* Browser : Browsers)
+	{
+		if (IsValid(Browser))
+		{
+			Browser->HandleOnAccessToken(*TokenBox->GetText().ToString());
+		}
+	}
+
+	HomeBrowser->LoadURL(TEXT("https://stage-home.zurutech.online/?version=504&platform=editor&dcversion=v0.0.1"));
+	ChatBrowser->LoadURL(TEXT("https://stage-chat.zurutech.online/"));
+	StoreBrowser->LoadURL(TEXT("https://stage-website-2024.zurutech.online/store"));
+	AccountBrowser->LoadURL(TEXT("https://stage-accounts.zurutech.online/"));
+	NotificationBrowser->LoadURL(TEXT("https://stage-notification.zurutech.online/"));
+	PublishBrowser->LoadURL(TEXT("https://stage-home.zurutech.online/publish/404"));
+	OrderBrowser->LoadURL(TEXT("https://stage-home.zurutech.online/my-orders"));
+	ProjectHistoryBrowser->LoadURL(TEXT("https://stage-home.zurutech.online/version-history/404"));
+	OrderSummaryBrowser->LoadURL(TEXT("https://stage-home.zurutech.online/cart/404"));
+	ObjectBrowser->LoadURL(TEXT("https://assets-v2.zurutech.online/#/object-library"));
+	MEPBrowser->LoadURL(TEXT("https://assets-v2.zurutech.online/#/fixture-library"));
+	MaterialBrowser->LoadURL(TEXT("https://assets-v2.zurutech.online/#/material-library"));
+	QeraBrowser->LoadURL(TEXT("https://stage-home.zurutech.online/qera/"));
 }
 
+void UMainUI::handleChatButtonClick()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Chat"));
+
+	if (IsValid(ChatBrowser)) {
+		ChatBrowser->IsVisible() ? ChatBrowser->HideBrowser() : ChatBrowser->ShowBrowser();
+	}
+}
+
+void UMainUI::handleHomeButtonClick()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Home"));
+
+	if (IsValid(HomeBrowser)) {
+		HomeBrowser->IsVisible() ? HomeBrowser->HideBrowser() : HomeBrowser->ShowBrowser();
+	}
+}
+
+void UMainUI::handleStoreButtonClick()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Store"));
+	if (IsValid(StoreBrowser)) {
+		StoreBrowser->IsVisible() ? StoreBrowser->HideBrowser() : StoreBrowser->ShowBrowser();
+	}
+}
+
+void UMainUI::handleAccountButtonClick()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Account"));
+	if (IsValid(AccountBrowser)) {
+		AccountBrowser->IsVisible() ? AccountBrowser->HideBrowser() : AccountBrowser->ShowBrowser();
+	}
+}
+
+void UMainUI::handleNotificationButtonClick()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Notification"));
+	if (IsValid(NotificationBrowser)) {
+		NotificationBrowser->IsVisible() ? NotificationBrowser->HideBrowser() : NotificationBrowser->ShowBrowser();
+	}
+}
+
+void UMainUI::handlePublishButtonClick()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Publish"));
+	if (IsValid(PublishBrowser)) {
+		PublishBrowser->IsVisible() ? PublishBrowser->HideBrowser() : PublishBrowser->ShowBrowser();
+	}
+}
+
+void UMainUI::handleOrderButtonClick()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Order"));
+	if (IsValid(OrderBrowser)) {
+		OrderBrowser->IsVisible() ? OrderBrowser->HideBrowser() : OrderBrowser->ShowBrowser();
+	}
+}
+
+void UMainUI::handleProjectHistoryButtonClick()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Project History"));
+	if (IsValid(ProjectHistoryBrowser)) {
+		ProjectHistoryBrowser->IsVisible() ? ProjectHistoryBrowser->HideBrowser() : ProjectHistoryBrowser->ShowBrowser();
+	}
+}
+
+void UMainUI::handleOrderSummaryButtonClick()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Order Summary"));
+	if (IsValid(OrderSummaryBrowser)) {
+		OrderSummaryBrowser->IsVisible() ? OrderSummaryBrowser->HideBrowser() : OrderSummaryBrowser->ShowBrowser();
+	}
+}
+
+void UMainUI::handleObjectButtonClick()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Object"));
+	if (IsValid(ObjectBrowser)) {
+		ObjectBrowser->IsVisible() ? ObjectBrowser->HideBrowser() : ObjectBrowser->ShowBrowser();
+	}
+}
+
+void UMainUI::handleMEPButtonClick()
+{
+	UE_LOG(LogTemp, Warning, TEXT("MEP"));
+	if (IsValid(MEPBrowser)) {
+		MEPBrowser->IsVisible() ? MEPBrowser->HideBrowser() : MEPBrowser->ShowBrowser();
+	}
+}
+
+void UMainUI::handleMaterialButtonClick()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Material"));
+	if (IsValid(MaterialBrowser)) {
+		MaterialBrowser->IsVisible() ? MaterialBrowser->HideBrowser() : MaterialBrowser->ShowBrowser();
+	}
+}
+
+void UMainUI::handleQeraButtonClick()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Qera"));
+	if (IsValid(QeraBrowser)) {
+		QeraBrowser->IsVisible() ? QeraBrowser->HideBrowser() : QeraBrowser->ShowBrowser();
+	}
+}
--- Source/CEF_Demo/Public/MainUI.h
+++ Source/CEF_Demo/Public/MainUI.h
@@ -6,12 +6,12 @@
 #include "Blueprint/UserWidget.h"
 #include <Components/Button.h>
 #include <Components/EditableText.h>
+#include "BrowserWidget.h"
 #include <DCWebBrowser.h>
 
 #include "MainUI.generated.h"
 
 DECLARE_DELEGATE_OneParam(FOnAccessToken, const FString&);
-
 
 class UHomeBrowser;
 class UChatBrowser;
@@ -35,6 +35,40 @@ public:
 	UHomeBrowser* HomeBrowser;
 
 	UPROPERTY(meta = (BindWidget))
+	UBrowserWidget* StoreBrowser;
+
+	UPROPERTY(meta = (BindWidget))
+	UBrowserWidget* AccountBrowser;
+
+	UPROPERTY(meta = (BindWidget))
+	UBrowserWidget* NotificationBrowser;
+
+	UPROPERTY(meta = (BindWidget))
+	UBrowserWidget* PublishBrowser;
+
+	UPROPERTY(meta = (BindWidget))
+	UBrowserWidget* OrderBrowser;
+
+	UPROPERTY(meta = (BindWidget))
+	UBrowserWidget* ProjectHistoryBrowser;
+
+	UPROPERTY(meta = (BindWidget))
+	UBrowserWidget* OrderSummaryBrowser;
+
+	UPROPERTY(meta = (BindWidget))
+	UBrowserWidget* ObjectBrowser;
+
+	UPROPERTY(meta = (BindWidget))
+	UBrowserWidget* MEPBrowser;
+
+	UPROPERTY(meta = (BindWidget))
+	UBrowserWidget* MaterialBrowser;
+
+	UPROPERTY(meta = (BindWidget))
+	UBrowserWidget* QeraBrowser;
+
+	// Buttons for all browsers
+	UPROPERTY(meta = (BindWidget))
 	UButton* ChatButton{};
 
 	UPROPERTY(meta = (BindWidget))
@@ -42,6 +76,39 @@ public:
 
 	UPROPERTY(meta = (BindWidget))
 	UButton* HomeButton{};
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* StoreButton{};
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* AccountButton{};
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* NotificationButton{};
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* PublishButton{};
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* OrderButton{};
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* ProjectHistoryButton{};
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* OrderSummaryButton{};
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* ObjectButton{};
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* MEPButton{};
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* MaterialButton{};
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* QeraButton{};
 
 	UPROPERTY(meta = (BindWidget))
 	UEditableText* TokenBox{};
@@ -52,6 +119,11 @@ protected:
 
 private:
 
+	bool _isChatLoaded{};
+	bool _isHomeLoaded{};
+
+	bool _isTokenUpdated{};
+
 	UFUNCTION()
 	void handleChatButtonClick();
 
@@ -61,4 +133,36 @@ private:
 	UFUNCTION()
 	void handleSaveButtonClick();
 
+	UFUNCTION()
+	void handleStoreButtonClick();
+
+	UFUNCTION()
+	void handleAccountButtonClick();
+
+	UFUNCTION()
+	void handleNotificationButtonClick();
+
+	UFUNCTION()
+	void handlePublishButtonClick();
+
+	UFUNCTION()
+	void handleOrderButtonClick();
+
+	UFUNCTION()
+	void handleProjectHistoryButtonClick();
+
+	UFUNCTION()
+	void handleOrderSummaryButtonClick();
+
+	UFUNCTION()
+	void handleObjectButtonClick();
+
+	UFUNCTION()
+	void handleMEPButtonClick();
+
+	UFUNCTION()
+	void handleMaterialButtonClick();
+
+	UFUNCTION()
+	void handleQeraButtonClick();
 };
